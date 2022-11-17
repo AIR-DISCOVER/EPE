4dffbeadf2934c71f34cb23a5eb3a102349ff2b4
GasaiYU <gaomingju19@malis.ucas.ac.cn>
2022-11-17 22:55:42 +0800

    add bash and config

--- bash_scripts/overfitting.sh
+++ bash_scripts/overfitting.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=5
 python3 /home/gaomx/epe/EPE/epe/EPEExperiment.py\
         --log_dir /home/gaomx/epe/EPE/saved_tasks/carla-overfitting/logs\
          $1 /home/gaomx/epe/EPE/config/overfitting.yaml
---
+++ bash_scripts/overfitting_test.sh
@@ -0,0 +1,5 @@
+export CUDA_VISIBLE_DEVICES=5
+python3 /home/gaomx/epe/EPE/epe/EPEExperiment.py\
+        --test_save_dir=/home/gaomx/epe/EPE/saved_tasks/infer\
+        --test_file_path=/home/gaomx/epe/EPE/data/file_lists/fake_dataset/Carla/file_overfitting_test.txt\
+         $1 /home/gaomx/epe/EPE/config/overfitting.yaml
